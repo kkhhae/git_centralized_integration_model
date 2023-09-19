c0da378493b8cc03185b2f3d6efa32ce8c1e3cf6
kkhhae <rlagodh3662@gmail.com>
2023-09-19 14:43:07 +0900

    .

--- Calculator.java
+++ Calculator.java
@@ -2,8 +2,8 @@ public class Calculator {
     public static double performOperation(double num1, double num2, char operator) {
 
 	//Task1
-	//Task2
-	//Task1
+	//T2sk2
+	//T3sk1
 	//Task5
     }
 }
