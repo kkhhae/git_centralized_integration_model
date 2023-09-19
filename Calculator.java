4c53e6c9fc05d2f29f842f92174bfe64b7523575
kkhhae <rlagodh3662@gmail.com>
2023-09-19 13:04:06 +0900

    .

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,6 @@
 public class Calculator {
     public static double performOperation(double num1, double num2, char operator) {
+	//Task2
+
+    }
 }
--- CalculatorInput.java
+++ CalculatorInput.java
@@ -1,3 +1,8 @@
 public class Calculator {
     public static double performOperation(double num1, double num2, char operator) {
+	
+	//Task1
+
+
+    }
 }
